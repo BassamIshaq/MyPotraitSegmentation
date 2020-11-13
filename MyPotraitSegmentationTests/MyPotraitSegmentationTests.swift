0c12c15793cacda58eea42f96cfc52dd85029707
BassamIshaq <40186285+BassamIshaq@users.noreply.github.com>
2020-11-13 10:50:45 +0500

    Add files via upload

---
+++ MyPotraitSegmentationTests/MyPotraitSegmentationTests.swift
@@ -0,0 +1,34 @@
+//
+//  MyPotraitSegmentationTests.swift
+//  MyPotraitSegmentationTests
+//
+//  Created by Bassam Ishaq on 04/10/2020.
+//  Copyright © 2020 Bassam Ishaq. All rights reserved.
+//
+
+import XCTest
+@testable import MyPotraitSegmentation
+
+class MyPotraitSegmentationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
